7427decabf2a734966e9af52292552f1f3ff8947
Andras Bali <andras.bali@gmail.com>
2020-05-28 19:54:59 +0200

    Initial commit.

---
+++ precache-manifest.6831785702aaec4a4bb4f0e27922d6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7875e1a6394c0814deb0f0db17bc20b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04bcc7b788ca2e434dd3",
+    "url": "/static/css/main.f6a3ddd9.chunk.css"
+  },
+  {
+    "revision": "7fca5ee52a9ed25ec02f",
+    "url": "/static/js/2.de1db28f.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.de1db28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bcc7b788ca2e434dd3",
+    "url": "/static/js/main.be48dad1.chunk.js"
+  },
+  {
+    "revision": "5c174c100b5769cb7e19",
+    "url": "/static/js/runtime-main.00dc4098.js"
+  }
+]);
